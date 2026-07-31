43e485384ab5abc0a5de8ed52dc939262ccea537
vijayprakash-hacker <vijay.prakash.212006@gmail.com>
2026-07-31 22:02:55 +0530

    Time: 0 ms (100.00%) | Memory: 42 MB (73.40%) - LeetSync

---
+++ 70-climbing-stairs/climbing-stairs.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int climbStairs(int n) {
+        if (n == 1) {
+            return 1;
+        }
+
+        int n1 = 1, n2 = 1;
+
+        for (int i = 2; i <= n; i++) {
+            n2 += n1;
+            n1 = n2 - n1;
+        }
+
+        return n2;
+    }
+}
