28dff777ccc029234239a09bccc56ec07a045827
vijayprakash-hacker <vijay.prakash.212006@gmail.com>
2026-09-16 19:20:18 +0530

    Time: 314 ms (58.37%) | Memory: 0B (100.00%) - LeetSync

--- 595-big-countries/big-countries.sql
+++ 595-big-countries/big-countries.sql
@@ -4,4 +4,11 @@ SELECT
 FROM
     World
 WHERE
-    area >= 3000000 Or population >= 25000000
+    area >= 3000000
+UNION
+SELECT
+    name, population, area
+FROM
+    World
+WHERE
+    population >= 25000000
